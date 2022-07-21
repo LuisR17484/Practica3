fe02bf920b77a5068e4dd477a7077a8d0c7ab775
Jose Luis Rivera <jrivera17484@plataforma-utslp.net>
2022-07-21 02:40:52 -0500

    Tercer avance

---
+++ js/Acciones.js
@@ -0,0 +1,12 @@
+alert('Hola');
+function suma() {
+	var numero1 = parseFloat(document.getElementById('numero1').value);/*('numero1')* -> es la variable id del archivo Calculadora.html*/
+	var numero2 = parseFloat(document.getElementById('numero2').value);
+
+	var resultado = numero1 + numero2;
+
+	document.getElementById('resultado').value = resultado;
+}
+
+
+
